45a204de13d797690f21edba0050059e9bb32de2
tonymcola <70962565+tonymcola@users.noreply.github.com>
2020-09-08 09:50:50 -0500

    Add files via upload

---
+++ main.java
@@ -0,0 +1,18 @@
+public class main {
+	public main(String[] args) {
+		int count = 0;
+		String strBible = "Mark";
+		strBible = strBible.toLowerCase();
+				
+		for (int = 0; 1< strBible.length(); i++) {
+			if (strBible.charAt(i) == 'a' || strBible.charAt(1) == 'e' || strBible.charAt(i) == 'i')
+				|| strBible.charAt(i) == 'o' || strBible.charAt(i) == 'u') }
+				count++;
+	}
+			
+		}
+		System.out.println("There are " + count + " vowels in your New Testiment Chapter title");
+		
+	}
+	
+}
